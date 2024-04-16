7f398bae7bfa3485f961ff185942b122b9b184a9
KulsumShaikh16 <160030040+KulsumShaikh16@users.noreply.github.com>
2024-04-16 08:43:14 -1200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+import chalk from "chalk";
+
+console.log(chalk.greenBright.bold("\n welcome to 'code with KULSUM' - Currency Converter \n"))
+
+//define the list of currencies and their exchange rates
+
+let exchange_rate:any={
+    "USD":1,
+    "EUR":0.9,
+    "JYP":113,
+    "CAD":1.3,
+    "AUD":1.65,
+    "PKR":277.70,
+}
+
+//User yo select currency to convert
+
+let user_answer=await inquirer.prompt([{
+    name:"from_currency",
+    type:"list",
+    message:"Select the Currency to Convert From",
+    choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+
+},
+   {
+    name:"to_currency",
+    type:"list",
+    message:"Select the Currency to Covert into",
+    choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+   },
+   {
+    name:"amount",
+    type:"input",
+    message:"Enter the Amount To Convert",
+   }
+])
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount= exchange_rate[user_answer.to_currency];
+let amount= user_answer.amount
+
+
+//formula of conversion
+let base_amount = amount/from_amount
+let converted_amount = base_amount* to_amount
+
+
+//display the converted amount
+console.log(chalk.redBright.bold(`Converted-Amount = ${converted_amount.toFixed(2)}`));
